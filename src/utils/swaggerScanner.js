1dd47f141b65e41e34a42d91f4e4e6cf4bb80fee
zhouli <2565510046@qq.com>
2018-10-20 23:57:04 +0800

    a

--- src/utils/swaggerScanner.js
+++ src/utils/swaggerScanner.js
@@ -1,3 +1,11 @@
 //通过模型与路由
 //生成文档
 //        "operationId": "addPet",代表唯一操作属性
+//   {
+// "name": "user",
+//     "description": "对用户的CURD",
+//     "externalDocs": {
+//     "description": "更多",
+//         "url": "/swagger-docs/doc-user.json"//可以单独生成静态文件
+// }
+// },
